b8e9f91dac98eff01a9f97d02f13d01f87823ca2
Bladimir Hernandez <hernandezblad95@gmail.com>
2018-06-19 17:21:14 -0500

    added the compoenent LandingPage. It renders html when the component is called

---
+++ react-app/src/LandingPage.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class LandingPage extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src='/logo.svg' className="App-logo" alt="logo" />
+                    <h1 className="App-title">Social-J [hard-coded]</h1>
+                </header>
+                <p className="App-intro">
+                    Social-J is a social media application where users can discuss a topic chosen by the site. 
+                </p>
+            </div>
+        );
+    }
+}
+
+export default LandingPage;
