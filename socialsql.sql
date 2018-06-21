6e01ee8fa66101adbbace067ddb9feec533b98b3
rocobiz <32499355+rocobiz@users.noreply.github.com>
2018-06-20 21:34:29 -0500

    update sql

--- socialsql.sql
+++ socialsql.sql
@@ -4,3 +4,11 @@ CREATE TABLE `socialjinfo` (
   `description` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO `sys`.`socialjinfo`
+(`id`,
+`title`,
+`description`)
+VALUES
+(1,
+'SocialJ App',
+'Group Project for SocialJ');
