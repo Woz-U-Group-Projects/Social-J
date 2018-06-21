9fabc99eba3a457f20ceff3c2144b39fd17769c2
rocobiz <32499355+rocobiz@users.noreply.github.com>
2018-06-20 21:06:30 -0500

    database script
    database script

---
+++ socialsql.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `socialjinfo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) DEFAULT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
