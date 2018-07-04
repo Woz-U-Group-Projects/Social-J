9d43cde06095cbf9578239013b907b106df7315f
Bladimir Hernandez <hernandezblad95@gmail.com>
2018-07-03 22:55:33 -0500

    -LandingPage content
    moved the contents of App.js into LandingPage.js

---
+++ react-app/src/LandingPage.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import "./App.css";
+import SocialjList from "./SocialjList";
+import axios from "axios";
+const URL = "http://localhost:8080/api/socialjinfos";
+
+
+class LandingPage extends Component {
+
+    state = {
+        socialjinfos: []
+    };
+
+    componentDidMount() {
+        axios.get(URL)
+            .then(response => {
+                const newList = response.data.map(c => {
+                    console.log(newList)
+                    return {
+                        id: c.id,
+                        title: c.title,
+                        description: c.description
+                    };
+                });
+
+                const newState = Object.assign({}, this.state, {
+                    socialjinfos: newList
+                });
+
+                this.setState(newState);
+            })
+            .catch(error => console.log(error));
+
+
+
+    }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <SocialjList socialjinfos={this.state.socialjinfos} />
+                </header>
+            </div>
+        );
+    }
+}
+export default LandingPage;
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import LandingPage from './LandingPage';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<LandingPage />, document.getElementById('root'));
-// registerServiceWorker();
+registerServiceWorker();
